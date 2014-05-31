f095dcb784ce04defaad0e767ab9b3225570563c
Yuriy Senko <ysenko@lohika.com>
2014-05-31 18:05:19 +0300

    Configuration added.

--- twitter_explorer/application.py
+++ twitter_explorer/application.py
@@ -1,4 +1,19 @@
 from flask import Flask
 
 
+CUSTOM_CONFIG_VAR = 'TWITER_EXPLORER_CONF'
+
+
+def load_config(app_obj, custom_conf_var=None):
+    """Load configuration for given Flask application.
+
+    Load base configuration from config folder and then load custom config
+    file (if exists).
+    """
+    app.config.from_pyfile('config/config.py', silent=False)
+    if custom_conf_var is not None:
+        app.config.from_envvar(custom_conf_var, silent=True)
+
+
 app = Flask(__name__)
+load_config(app, CUSTOM_CONFIG_VAR)
