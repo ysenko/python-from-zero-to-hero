44231dc70e0d97d36c9cdbd1485b452061074350
Yuriy Senko <ysenko@lohika.com>
2014-05-31 20:18:02 +0300

    Base template added.

--- twitter_explorer/__init__.py
+++ twitter_explorer/__init__.py
@@ -1,11 +1,24 @@
+import functools
+
+from flask import render_template as flask_render_template, url_for
+
 from application import app
+
+
+def render_template(template, *args, **kwargs):
+    kwargs.setdefault('title', 'Twitterxplorer v0.1')
+    kwargs.setdefault('bootstrap_path', url_for('static',
+                                                filename='bootstrap-3.1.1-dist'))
+
+    print 'PATH', kwargs['bootstrap_path']
+    return flask_render_template(template, *args, **kwargs)
 
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('base.html')
 
 
 # Debug only.
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
