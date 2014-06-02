8c4bdae4d2ee2054349f2a8b721cb9474afc11ae
Yuriy Senko <ysenko@lohika.com>
2014-06-02 17:44:33 +0000

    Show proper page title.

--- twitter_explorer/__init__.py
+++ twitter_explorer/__init__.py
@@ -1,16 +1,13 @@
-import functools
-
 from flask import render_template as flask_render_template, url_for
 
 from application import app
 
 
 def render_template(template, *args, **kwargs):
-    kwargs.setdefault('title', 'Twitterxplorer v0.1')
-    kwargs.setdefault('bootstrap_path', url_for('static',
-                                                filename='bootstrap-3.1.1-dist'))
+    kwargs.setdefault('page_title', 'TwitterExplorer v0.1')
+    kwargs.setdefault('bootstrap_path',
+                      url_for('static', filename='bootstrap-3.1.1-dist'))
 
-    print 'PATH', kwargs['bootstrap_path']
     return flask_render_template(template, *args, **kwargs)
 
 
