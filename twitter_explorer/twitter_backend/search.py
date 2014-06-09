b51f1d2c9ef01ffc369052efceddfa86c8a912f7
Yuriy Senko <ysenko@lohika.com>
2014-06-09 15:20:58 +0300

    Render google link instead of coordinates.

--- twitter_explorer/twitter_backend/search.py
+++ twitter_explorer/twitter_backend/search.py
@@ -1,5 +1,7 @@
 """Twitter search related utilities."""
 
+import urllib
+
 
 def _parse_search(search_results):
     """Parse search results and return a list of dicts. Each dict has the
@@ -15,11 +17,23 @@ def _parse_search(search_results):
         {
             'text': t.text,
             'author': t.author.screen_name,
-            'geo_data': None
+            'geo_data': get_map_for_geo(t.geo.get('coordinates'))
+                        if t.geo else None
         }
         for t in search_results]
+
 
 
 def search(api, query, count=50):
     """Search for tweets and return parsed data."""
     return _parse_search(api.search(query, count=count))
+
+
+def get_map_for_geo(coordinates):
+    """Return a Google Map URL for the given coordinates."""
+    params = {
+        'q': ','.join(str(p) for p in coordinates)
+    }
+    url = 'http://google.com/search?%s' % (urllib.urlencode(params))
+    print url
+    return url
