7a9168557104a641990a6bd5f9e95b370bc040fe
Yuriy Senko <ysenko@lohika.com>
2014-06-09 13:45:59 +0300

    Index page. Search form added.

--- twitter_explorer/handlers/index.py
+++ twitter_explorer/handlers/index.py
@@ -1,11 +1,25 @@
 import flask
 
 from flask.ext.login import login_required
+from flask_wtf import Form
+from wtforms import TextField
+from wtforms.validators import DataRequired
 
+from twitter_explorer.twitter_backend import auth
 from twitter_explorer.utils import render_template
 
 
+class SearchForm(Form):
+    search_query = TextField('search_query', validators=[DataRequired()])
+
 @login_required
 def index():
-    if flask.request.method == 'GET':
-        return render_template('index.html', current_page='Main')
+    search_results = []
+    search_query = None
+
+    if flask.request.method == 'POST':
+        pass
+
+    return render_template('index.html', current_page='Main',
+                            search_results=search_results,
+                            search_query=search_query)
